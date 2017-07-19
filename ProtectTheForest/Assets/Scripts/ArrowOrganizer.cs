5357d1d0be3c463b99425dff028cc55c3444b86f
Rohit <rohit313.93@gmail.com>
2017-07-19 00:51:12 -0700

    update tension in string when pulling it back

--- ProtectTheForest/Assets/Scripts/ArrowOrganizer.cs
+++ ProtectTheForest/Assets/Scripts/ArrowOrganizer.cs
@@ -16,7 +16,10 @@ public class ArrowOrganizer : MonoBehaviour {
     public GameObject arrowStartPoint;
     public GameObject stringStartPoint;
     public GameObject stringMaxPull;
+    public GameObject quiver;
+
     private float dist;
+    private float maxDist;
 
     private bool isArrowAttached = false;
 
@@ -31,8 +34,8 @@ public class ArrowOrganizer : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
-		
-	}
+        maxDist = (stringStartPoint.transform.position - stringMaxPull.transform.position).magnitude;
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -45,7 +48,7 @@ public class ArrowOrganizer : MonoBehaviour {
     {
         if(currentArrow == null)
         {
-            currentArrow = Instantiate(arrowObject);//, trackedObj.transform.position, trackedObj.transform.rotation);
+            currentArrow = Instantiate(arrowObject);
             currentArrow.transform.parent = trackedObj.transform;
             currentArrow.transform.localPosition = new Vector3(0f, 0f, .342f);
             currentArrow.transform.localRotation = Quaternion.identity;
@@ -65,13 +68,23 @@ public class ArrowOrganizer : MonoBehaviour {
         if (isArrowAttached)
         {
             dist = (stringStartPoint.transform.position - trackedObj.transform.position).magnitude;
-            stringOnBow.transform.localPosition = stringStartPoint.transform.localPosition + new Vector3(3.5f * dist, 0f, 0f);
+            if(dist >= maxDist)
+            {
+                dist = maxDist;
+            }
+            stringOnBow.transform.localPosition = stringStartPoint.transform.localPosition + new Vector3(7f * dist, 0f, 0f);
 
             var device = SteamVR_Controller.Input((int)trackedObj.index);
             var otherDevice = SteamVR_Controller.Input((int)otherController.index);
-            ushort amountOfVibration = (ushort)(500 * dist);
+
+            ushort amountOfVibration = (ushort)(dist * 550);
+
             device.TriggerHapticPulse(amountOfVibration);
             otherDevice.TriggerHapticPulse(amountOfVibration);
+            // play sound of the string based on the speed that the string is being pulled back - the quicker it is the louder the noise
+
+            // figure out how to do the vibrations for when the string is at the max distance! --> the way they do in the lab is with it vibrating intermitantly 
+                    
             if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
             {
                 FireArrow();
@@ -93,7 +106,7 @@ public class ArrowOrganizer : MonoBehaviour {
         currentArrow.transform.parent = null;
         currentArrow.GetComponent<Arrow>().arrowIsFired();
         Rigidbody arrowRB = currentArrow.GetComponent<Rigidbody>();
-        arrowRB.velocity = currentArrow.transform.forward * 25f * dist;
+        arrowRB.velocity = currentArrow.transform.forward * 35f * dist;
         arrowRB.useGravity = true;
 
         stringOnBow.transform.position = stringStartPoint.transform.position; // reset the string so that the bow is ready to be used again
