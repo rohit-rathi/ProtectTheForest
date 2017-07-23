75b867a2543a1622f95768b857911816395d0dc6
Rohit <rohit313.93@gmail.com>
2017-07-22 20:33:24 -0700

    add fireballs to the game and have them fall on the island.

---
+++ ProtectTheForest/Assets/Scripts/FireSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpawner : MonoBehaviour {
+
+    float minCordinate = -17.5f;
+    float maxCordinate = 17.5f;
+    float heightOfSpawn = 600.0f;
+
+    public GameObject fireBall;
+    public GameObject fireOnLand;
+
+       
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("InstantiateFireBalls", 10, 5);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+
+    public void InstantiateFireBalls()
+    {
+        Instantiate(fireBall, new Vector3(Random.Range(minCordinate, maxCordinate), heightOfSpawn, Random.Range(minCordinate, maxCordinate)), Quaternion.identity);
+
+    }
+}
