6b5f9ab8ce1652d783c320e8c6bca2061ba21db6
Rohit <rohit313.93@gmail.com>
2017-08-17 00:02:53 -0700

    Need to add one more sound track for training scene

---
+++ ProtectTheForest/Assets/Scripts/MoveBucketIntroScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBucketIntroScene : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, 4 + Mathf.Sin(Time.time * 4.0f), transform.position.z);
+	}
+}
